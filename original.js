c2fb9b3787bfb550fcae1356293c8f1c21794fab
hpark <hpark11@wellesley.edu>
2018-03-29 01:25:59 -0400

    added arrows

---
+++ original.js
@@ -0,0 +1,71 @@
+var width = window.innerWidth * 0.8,
+    height = window.innerHeight;
+
+var svg = d3.select("#canvas").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var force = d3.layout.force()
+    .gravity(.01)
+    .distance(100)
+    .charge(-70)
+    .size([width, height]);
+
+d3.json("composers.json", function (json) {
+    force
+        .nodes(json.nodes)
+        .links(json.links)
+        .start();
+
+    var link = svg.selectAll(".link")
+        .data(json.links)
+        .enter().append("line")
+        .attr("class", "link")
+        .style("stroke-width", function (d) {
+            return d.weight;
+        });
+
+    var node = svg.selectAll(".node")
+        .data(json.nodes)
+        .enter().append("g")
+        .attr("class", "node")
+        .call(force.drag);
+
+    node.append("circle")
+        .attr("r", "30")
+
+    node.append("image")
+        .attr("href", function (d) {
+            return './img/' + d.name + '.png';
+        })
+        .attr('height', '60')
+        .attr('width', '60')
+        .attr('x', '-30px')
+        .attr('y', '-30px');
+
+    node.append("text")
+        .attr("dx", -30)
+        .attr("dy", "3em")
+        .text(function (d) {
+            return d.name
+        });
+
+    force.on("tick", function () {
+        link.attr("x1", function (d) {
+                return d.source.x;
+            })
+            .attr("y1", function (d) {
+                return d.source.y;
+            })
+            .attr("x2", function (d) {
+                return d.target.x;
+            })
+            .attr("y2", function (d) {
+                return d.target.y;
+            });
+
+        node.attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+    });
+});
--- script.js
+++ script.js
@@ -1,38 +1,76 @@
-var width = window.innerWidth * 0.8,
-    height = window.innerHeight;
+// get the data
+d3.csv("data.csv", function (error, links) {
 
-var svg = d3.select("#canvas").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    var nodes = {};
 
-var force = d3.layout.force()
-    .gravity(.01)
-    .distance(100)
-    .charge(-70)
-    .size([width, height]);
+    // Compute the distinct nodes from the links.
+    links.forEach(function (link) {
+        link.source = nodes[link.source] ||
+            (nodes[link.source] = {
+                name: link.source
+            });
+        link.target = nodes[link.target] ||
+            (nodes[link.target] = {
+                name: link.target
+            });
+        link.value = +link.value;
+    });
 
-d3.json("composers.json", function (json) {
-    force
-        .nodes(json.nodes)
-        .links(json.links)
+    var width = window.innerWidth * 0.8,
+        height = window.innerHeight,
+        radius = 30;
+
+    var force = d3.layout.force()
+        .nodes(d3.values(nodes))
+        .links(links)
+        .size([width, height])
+        .linkDistance(100)
+        .charge(-700)
+        .on("tick", tick)
         .start();
 
-    var link = svg.selectAll(".link")
-        .data(json.links)
-        .enter().append("line")
-        .attr("class", "link")
-        .style("stroke-width", function (d) {
-            return d.weight;
-        });
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
+    // build the arrow.
+    svg.append("svg:defs").selectAll("marker")
+        .data(["end"]) // Different link/path types can be defined here
+        .enter().append("svg:marker") // This section adds in the arrows
+        .attr("id", String)
+        .attr("viewBox", "0 -5 10 10")
+        .attr("refX", 43.3)
+        .attr("refY", 0)
+        .attr("markerWidth", 6)
+        .attr("markerHeight", 6)
+        .attr("orient", "auto")
+        .attr("fill", "#666")
+        .append("svg:path")
+        .attr("d", "M0,-5L10,0L0,5");
+
+    // add the links and the arrows
+    var path = svg.append("svg:g").selectAll("path")
+        .data(force.links())
+        .enter().append("svg:path")
+        .attr("class", function (d) {
+            return "link " + d.type;
+        })
+        .attr("marker-end", "url(#end)")
+        .on("mouseover", handleMouseOver)
+        .on("mouseout", handleMouseOut);
+
+    // define the nodes
     var node = svg.selectAll(".node")
-        .data(json.nodes)
+        .data(force.nodes())
         .enter().append("g")
         .attr("class", "node")
+        .on("click", click)
+        .on("dblclick", dblclick)
         .call(force.drag);
 
+    // add the nodes
     node.append("circle")
-        .attr("r", "30")
+        .attr("r", radius)
 
     node.append("image")
         .attr("href", function (d) {
@@ -43,29 +81,53 @@ d3.json("composers.json", function (json) {
         .attr('x', '-30px')
         .attr('y', '-30px');
 
+    // add the text
     node.append("text")
-        .attr("dx", -30)
-        .attr("dy", "3em")
+        .attr("x", 12)
+        .attr("dy", ".35em")
         .text(function (d) {
-            return d.name
+            return d.name;
         });
 
-    force.on("tick", function () {
-        link.attr("x1", function (d) {
-                return d.source.x;
-            })
-            .attr("y1", function (d) {
-                return d.source.y;
-            })
-            .attr("x2", function (d) {
-                return d.target.x;
-            })
-            .attr("y2", function (d) {
-                return d.target.y;
+    // add the curvy lines
+    function tick() {
+        path.attr("d", function (d) {
+            var dx = d.target.x - d.source.x,
+                dy = d.target.y - d.source.y,
+                dr = 0;
+            return "M" +
+                d.source.x + "," +
+                d.source.y + "A" +
+                dr + "," + dr + " 0 0,1 " +
+                d.target.x + "," +
+                d.target.y;
+        });
+
+        node
+            .attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
             });
+    }
 
-        node.attr("transform", function (d) {
-            return "translate(" + d.x + "," + d.y + ")";
+    function click() {}
+
+    function dblclick() {}
+
+    // Create Event Handlers for mouse
+    function handleMouseOver(d, i) { // Add interactivity
+
+        // Use D3 to select element, change color and size
+        d3.select(this).attr({
+            fill: "orange",
+            r: radius * 2
         });
-    });
+    }
+
+    function handleMouseOut(d, i) {
+        // Use D3 to select element, change color back to normal
+        d3.select(this).attr({
+            fill: "black",
+        });
+    }
+
 });
